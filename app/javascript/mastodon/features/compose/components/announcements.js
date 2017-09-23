9e50a2e2e6da85437857e86ed1f53dc29e2ca48a
unarist <m.unarist@gmail.com>
2017-09-24 00:11:05 +0900

    [Fix] アナウンスメント機能：　最新ファイルに置き換え（lindwurm/mastodon）

--- app/javascript/mastodon/features/compose/components/announcements.js
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -3,6 +3,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import Immutable from 'immutable';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Announcement extends React.PureComponent {
 
@@ -16,39 +17,26 @@ class Announcement extends React.PureComponent {
     const contents = [];
     contents.push(<div key='body' className='announcements__body'>{item.get('body')}</div>);
     if (item.get('icon')) {
-      const iconStyle = {
-        height: '40px',
-        width: '40px',
-        flex: '0 0 auto',
-        textAlign: 'center',
-      };
       contents.push(
-        <div key='icon' className='announcements__icon' style={iconStyle}>
-          <img src={item.get('icon')} alt='' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+        <div key='icon' className='announcements__icon'>
+          <img src={item.get('icon')} alt='' />
         </div>
       );
     }
 
     const href = item.get('href');
 
-    const style = {
-      display: 'flex',
-      padding: '10px',
-      margin: '10px',
-      backgroundColor: 'white',
-      color: '#313543',
-      boxShadow: '0 0 15px rgba(0,0,0,.2)',
-      borderRadius: '4px',
-      cursor: href ? 'pointer' : null,
-      textDecoration: 'none',
-    };
+    const classname = classnames({
+      'announcements__item': true,
+      'announcements__item--clickable': !!href,
+    });
 
     if (!href) {
-      return (<div className='announcements__item' style={style}>{contents}</div>);
+      return (<div className={classname}>{contents}</div>);
     } else if (href.startsWith('/web/')) {
-      return (<Link to={item.get('href').slice(4)} className='announcements__item' style={style}>{contents}</Link>);
+      return (<Link to={item.get('href').slice(4)} className={classname}>{contents}</Link>);
     } else {
-      return (<a href={item.get('href')} target='_blank' className='announcements__item' style={style}>{contents}</a>);
+      return (<a href={item.get('href')} target='_blank' className={classname}>{contents}</a>);
     }
   }
 
@@ -57,16 +45,24 @@ class Announcement extends React.PureComponent {
 export default class Announcements extends React.PureComponent {
 
   state = {
-    items: Immutable.Map(),
+    items: Announcements.cache || Immutable.Map(),
   }
+
+  static isCacheControlled = false
+  static lastDate = null
+  static cache = null
 
   constructor () {
     super();
     this.refresh();
   }
 
+  componentWillUnmount() {
+    this.cancelPolling();
+  }
+
   setPolling = () => {
-    this.timer = setTimeout(this.refresh, 60 * 1000);
+    this.timer = setTimeout(this.refresh, 2 * 60 * 1000);
   }
 
   cancelPolling = () => {
@@ -76,11 +72,35 @@ export default class Announcements extends React.PureComponent {
     }
   }
 
+  deleteServiceWorkerCache = () => {
+    // files in /system/ will be cached by SW
+    if (self.caches) {
+      return caches.open('mastodon-system')
+        .then(cache => cache.delete(window.origin + '/system/announcements.json'))
+        .catch(() => {});
+    } else {
+      return Promise.resolve();
+    }
+  }
+
   refresh = () => {
     this.timer = null;
-    axios.get('/system/announcements.json')
-    .then(resp => this.setState({ items: Immutable.fromJS(resp.data) }), () => {})
-    .then(this.setPolling);
+
+    axios.get('/announcements.json', {
+      headers: {
+        'If-Modified-Since': !Announcements.isCacheControlled && Announcements.lastDate || '',
+      },
+    })
+      .then(resp => {
+        Announcements.isCacheControlled = !!resp.headers['cache-control'];
+        Announcements.lastDate = resp.headers['last-modified'];
+        return resp;
+      })
+      .then(resp => this.setState({ items: Announcements.cache = Immutable.fromJS(resp.data) || {} }))
+      .catch(err => err.response.status !== 304 && console.warn(err))
+      .then(this.deleteServiceWorkerCache)
+      .then(this.setPolling)
+      .catch(err => err && console.warn(err));
   }
 
   render() {
@@ -89,9 +109,9 @@ export default class Announcements extends React.PureComponent {
     return (
       <ul className='announcements'>
         {items.entrySeq().map(([key, item]) =>
-          <li key={key}>
+          (<li key={key}>
             <Announcement item={item} />
-          </li>
+          </li>)
         )}
       </ul>
     );
