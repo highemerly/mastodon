a22b7e15cf03c271553f3a74a843111bdfd7207b
highemerly <high.emerly.ytfin@gmail.com>
2019-09-13 18:22:50 +0900

    [Change] 全文検索結果の表示数について，API上限値を変更
    - 全文検索の結果表示上限を20から60に変更
    - 全文検索の結果表示上限をAPIレート制限の影響を受けないように変更

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -3,7 +3,7 @@
 class Api::V2::SearchController < Api::BaseController
   include Authorization
 
-  RESULTS_LIMIT = 20
+  RESULTS_LIMIT = 60
 
   before_action -> { doorkeeper_authorize! :read, :'read:search' }
   before_action :require_user!
@@ -21,7 +21,7 @@ class Api::V2::SearchController < Api::BaseController
     SearchService.new.call(
       params[:q],
       current_account,
-      limit_param(RESULTS_LIMIT),
+      RESULTS_LIMIT,
       search_params.merge(resolve: truthy_param?(:resolve))
     )
   end
