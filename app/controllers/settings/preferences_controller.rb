e5de73c3cfab9b859441266ddb523895b6678b25
highemerly <high.emerly.ytfin@gmail.com>
2021-05-09 01:41:53 +0900

    [Add] HTL上にDMを表示しないオプションを追加（かつデフォルトに設定）
    - 3.3.0からHTL上にDMを表示するようになった
    - きもちわるいので3.2.x以前の表示に戻すオプションをつくり，かつそれを初期値に設定した
    
    Conflicts:
    	config/locales/simple_form.en.yml

--- app/controllers/settings/preferences_controller.rb
+++ app/controllers/settings/preferences_controller.rb
@@ -53,6 +53,7 @@ class Settings::PreferencesController < Settings::BaseController
       :setting_advanced_layout,
       :setting_use_blurhash,
       :setting_use_pending_items,
+      :setting_hidden_direct,
       :setting_trends,
       :setting_crop_images,
       notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account trending_tag),
