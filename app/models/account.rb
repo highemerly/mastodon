e99bb53996fc4b81de648cf9adcd9117ac16770b
highemerly <high.emerly.ytfin@gmail.com>
2021-05-09 01:40:28 +0900

    プロフィール絵文字を追加 (best-friends/mastodon)
    - カスタム絵文字としてユーザのアイコンが使える機能
    - 対象はトゥート，投票の選択肢，お知らせ，アカウントプロフィール欄
    - 自サーバの人が他サーバのアカウントのプロフィール絵文字を使うことはできる
    - 他サーバの人がプロフィール絵文字を使うことはできない

--- app/models/account.rb
+++ app/models/account.rb
@@ -530,7 +530,7 @@ class Account < ApplicationRecord
   end
 
   def emojis
-    @emojis ||= CustomEmoji.from_text(emojifiable_text, domain)
+    @emojis ||= CustomEmoji.from_text(emojifiable_text, domain) + profile_emojis
   end
 
   before_create :generate_keys
@@ -586,4 +586,6 @@ class Account < ApplicationRecord
 
     CanonicalEmailBlock.where(reference_account: self).delete_all
   end
+
+  include Friends::ProfileEmoji::AccountExtension
 end
