ccf4889bd8b34db3d1f721ae0f76084c0189ddce
highemerly <high.emerly.ytfin@gmail.com>
2022-03-31 01:12:47 +0900

    [Change] 動画・画像のアップロード制限を緩和
    Conflicts:
    	app/models/media_attachment.rb

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -38,8 +38,8 @@ class MediaAttachment < ApplicationRecord
 
   MAX_DESCRIPTION_LENGTH = 1_500
 
-  IMAGE_LIMIT = 10.megabytes
-  VIDEO_LIMIT = 40.megabytes
+  IMAGE_LIMIT = 15.megabytes
+  VIDEO_LIMIT = 65.megabytes
 
   MAX_VIDEO_MATRIX_LIMIT = 2_304_000 # 1920x1200px
   MAX_VIDEO_FRAME_RATE   = 60
@@ -68,7 +68,7 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 2_073_600, # 1920x1080px
+      pixels: 4_147_200, # 2880x1440px
       file_geometry_parser: FastGeometryParser,
     }.freeze,
 
