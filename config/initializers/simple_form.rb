99b766117d0553786a0acf64254a50617eba54d3
highemerly <high.emerly.ytfin@gmail.com>
2022-11-06 16:01:46 +0900

    [Change] はんドン独自機能と既存機能のコンパチをなんとかした
    Conflicts:
    	config/locales/simple_form.en.yml
    
    Conflicts:
    	config/locales/simple_form.ja.yml
    
    Conflicts:
    	app/javascript/styles/mastodon/accounts.scss
    	config/i18n-tasks.yml

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -19,8 +19,17 @@ module RecommendedComponent
   end
 end
 
+module HandononlyComponent
+  def handononly(wrapper_options = nil)
+    return unless options[:handononly]
+    options[:label_text] = ->(raw_label_text, _required_label_text, _label_present) { safe_join([raw_label_text, ' ', content_tag(:span, I18n.t('simple_form.handononly'), class: 'handononly')]) }
+    nil
+  end
+end
+
 SimpleForm.include_component(AppendComponent)
 SimpleForm.include_component(RecommendedComponent)
+SimpleForm.include_component(HandononlyComponent)
 
 SimpleForm.setup do |config|
   # Wrappers are used by the form builder to generate a
@@ -78,6 +87,7 @@ SimpleForm.setup do |config|
 
     b.wrapper tag: :div, class: :label_input do |ba|
       ba.optional :recommended
+      ba.optional :handononly
       ba.use :label
 
       ba.wrapper tag: :div, class: :label_input__wrapper do |bb|
