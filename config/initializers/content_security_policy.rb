b1e6ce26b9f170969d4b757309f1a60a33062682
highemerly <high.emerly.ytfin@gmail.com>
2023-01-22 22:53:26 +0900

    instancetickerのホスト名変更に対応

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -19,8 +19,7 @@ media_host ||= host_to_url(ENV['AZURE_ALIAS_HOST'])
 media_host ||= host_to_url(ENV['S3_HOSTNAME']) if ENV['S3_ENABLED'] == 'true'
 media_host ||= assets_host
 
-instance_ticker_host     = 'https://inst.ance.tk'
-instance_ticker_img_host = 'https://itk.pw'
+instance_ticker_host     = 'https://34.si'
 
 def sso_host
   return unless ENV['ONE_CLICK_SSO_LOGIN'] == 'true'
@@ -45,7 +44,7 @@ Rails.application.config.content_security_policy do |p|
   p.default_src     :none
   p.frame_ancestors :none
   p.font_src        :self, assets_host
-  p.img_src         :self, :https, :data, :blob, assets_host, instance_ticker_img_host
+  p.img_src         :self, :https, :data, :blob, assets_host, instance_ticker_host
   p.style_src       :self, assets_host, instance_ticker_host
   p.media_src       :self, :https, :data, assets_host
   p.frame_src       :self, :https
