a4b3b9eec32d8bb11d57598913b073e2d778da69
highemerly <highemerly@me.com>
2023-11-17 16:07:05 +0900

    [tuning] decrease trend threshold to match handon.club speeds
    - tags: 3
    - links/statuses: 4

--- app/models/trends/tags.rb
+++ app/models/trends/tags.rb
@@ -4,8 +4,8 @@ class Trends::Tags < Trends::Base
   PREFIX = 'trending_tags'
 
   self.default_options = {
-    threshold: 5,
-    review_threshold: 3,
+    threshold: 3,
+    review_threshold: 2,
     max_score_cooldown: 2.days.freeze,
     max_score_halflife: 4.hours.freeze,
     decay_threshold: 1,
