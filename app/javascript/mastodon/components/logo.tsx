3b2262b5d4304e8e3553d969d76dc4b5a1eeaad2
highemerly <highemerly@me.com>
2023-11-17 16:17:51 +0900

    [add] change brandinig logos
    thx @inabap

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,8 +1,8 @@
 import logo from 'mastodon/../images/logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
+  <svg viewBox='0 0 2500 617' className='logo logo--wordmark' role='img'>
+    <title>handon.club</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
 );
