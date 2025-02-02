19b6532dc102c25e736a6e09d6920e7f37c6f2c7
highemerly <highemerly@me.com>
2025-02-02 23:41:45 +0900

    [add] add feature of DM visibility at HTL
    - In vanilla mastodon, private messages (formaly direct messages) will appear to home timeline.
    - However, handon.club users does not like this behaivors.
    - Add options to remove private messages from home timelines. Default: true.

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -28,6 +28,7 @@ class InitialStateSerializer < ActiveModel::Serializer
       store[:advanced_layout]   = object_account_user.setting_advanced_layout
       store[:use_blurhash]      = object_account_user.setting_use_blurhash
       store[:use_pending_items] = object_account_user.setting_use_pending_items
+      store[:hidden_direct]     = object_account_user.hidden_direct?
       store[:show_trends]       = Setting.trends && object_account_user.setting_trends
     else
       store[:auto_play_gif] = Setting.auto_play_gif
