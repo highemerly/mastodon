5cc6b313569251a0ee46ff2f1fbaf1edba01a42b
highemerly <highemerly@me.com>
2025-11-11 21:50:56 +0900

    add handon-local-account class

--- app/javascript/mastodon/components/display_name/default.tsx
+++ app/javascript/mastodon/components/display_name/default.tsx
@@ -44,6 +44,11 @@ export const DisplayNameDefault: FC<
 > = ({ account, localDomain, className, ...props }) => {
   const username = useAccountHandle(account, localDomain);
 
+  let classname = 'display-name__account'
+  if (localDomain) {
+    classname = 'display-name__account handon-local-account'
+  }
+
   return (
     <DisplayNameWithoutDomain
       account={account}
@@ -51,7 +56,7 @@ export const DisplayNameDefault: FC<
       {...props}
     >
       {' '}
-      <span className='display-name__account'>
+      <span className={classname}>
         {username ?? <Skeleton width='7ch' />}
       </span>
     </DisplayNameWithoutDomain>
